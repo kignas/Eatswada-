d346516e1721796f1876455364ca737733d8bac1
Sagnik <sagnikroysagnikroy4@gmail.com>
2026-04-30 18:17:13 +0530

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
-const { getAllRestaurants, createMasterItem } = require('../controllers/adminController');
+const { getAllRestaurants, createMasterItem, getRestaurantMenu, deleteMenuItem } = require('../controllers/adminController');
 
 router.get('/restaurants', protect, getAllRestaurants);
 router.post('/menu', protect, createMasterItem);
+
+// 🚨 THE NEW ROUTES FOR VIEWING AND DELETING ITEMS
+router.get('/menu/:restaurantId', protect, getRestaurantMenu);
+router.delete('/menu/:itemId', protect, deleteMenuItem);
 
 module.exports = router;
