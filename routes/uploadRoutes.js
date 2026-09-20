cfb1465acd5c5270bccff9cfd119e950e1b21f7e
Sagnik <sagnikroysagnikroy4@gmail.com>
2026-09-20 11:30:06 +0530

    Add files via upload

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -26,7 +26,7 @@ const handleUpload = (req, res, next) => {
 };
 
 // @route   POST /api/upload/:type
-// @desc    Upload an image to Cloudinary. :type is one of restaurants | menu | categories | banners
+// @desc    Upload an image to Cloudinary. :type is one of restaurants | menu | categories | banners | restaurantDelivery
 // @access  Private (admin)
 router.post('/:type', protect, authorize('admin'), handleUpload, uploadImage);
 
